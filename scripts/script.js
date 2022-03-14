15081d7113e281148fda03b02390824846da2cda
masalamind <themasalacollective@gmail.com>
2022-03-14 10:43:02 +0300

    Added a form button and submit functions

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,35 @@ $(".portfolio-item").click(function(){
     
 })
 
+$(document).ready(function() {
 
+    // alert("Outside submit function is working");
+  
+    $("#emailForm").submit(function(e) {
+      
+        let name =  $("#name").val(); 
+        let email = $("#email").val(); 
+        let message = $("textarea").val();
+
+
+        console.log("name: "+ name + " email: "+ email + "message: " + message);
+      
+    //   // alert("submit start is working");
+      
+    //   blanks.forEach(function(blank){ 
+  
+    //     // debugger;
+    //     var userInput = $("input#" + blank).val();
+    //     $("." + blank).text(userInput).val();
+    //   });
+      
+      // alert("submit end is working");
+  
+      e.preventDefault();
+  
+    });
+  });
+  
 
 
 

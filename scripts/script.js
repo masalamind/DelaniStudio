7e1c362b428965aadb13f945cc5c86c6d4e36a83
masalamind <themasalacollective@gmail.com>
2022-03-14 16:11:31 +0300

    Commit cleans structure

--- scripts/script.js
+++ scripts/script.js
@@ -30,10 +30,10 @@ $(document).ready(function() {
         var alertTrigger = document.getElementById('email-btn')
 
         function alert(message, type) {
+
             var wrapper = document.createElement('div')
             wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
             alertPlaceholder.append(wrapper)
-
         }
 
         if (alertTrigger) {
@@ -41,9 +41,8 @@ $(document).ready(function() {
             alert(name + " ,we have received your message. Thank you for reaching out to us", 'success');
 
             })
-        }    
+        }
     
-  
         e.preventDefault();
   
     });

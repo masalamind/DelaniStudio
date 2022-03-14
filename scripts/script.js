52d81df3438bf1f2f0c81be0583d0ca358aa4103
masalamind <themasalacollective@gmail.com>
2022-03-14 09:13:04 +0300

    The animations for portfolio items added

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,24 @@
-$(".card-img").click(function(){    
-      
-       
-    $(".card-text").show(1000);   
 
+
+$(".portfolio-item").click(function(){     
+    
+    let termId = $(this).attr("id");
+    let termDefinitionId = termId + "-desc";
+
+   
+    console.log($('#' + termDefinitionId));    
+    $('#' + termDefinitionId).show(1000);   
 
 
 })
 
 
 
+
+$(".card-text").click(function(){     
+    
+    let termId = $(this).attr("id");
+    $('#' + termId).hide(1000);   
+   
+
+})
